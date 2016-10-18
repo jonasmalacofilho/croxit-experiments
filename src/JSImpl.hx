ddb91a1b971b7f33870ee53e828f4caef7ff15f6
lehonma <caio.honma@gmail.com>
2016-10-18 15:02:06 -0200

    It builds a JAR!

---
+++ src/JSImpl.hx
@@ -0,0 +1,20 @@
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+class JSImpl {
+
+var ctx : Context;
+
+public function new(c : Context)
+{
+    this.ctx = c;
+}
+
+@JavaScriptInterface
+public function showMessage(msg : String)
+{
+    Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
+}
+
+}
